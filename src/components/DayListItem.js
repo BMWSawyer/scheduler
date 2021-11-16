e48183d2f3f039d5b9755c80785e627e533010f7
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-11-15 19:57:11 -0800

    Add tests and format string responses

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,11 +8,29 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": (props.spots === 0)
   });
+
+  function formatSpots(spots) {
+    let stringReponse = '';
+
+    if (spots === 0) {
+      stringReponse += 'no spots remaining';
+    }
+
+    if (spots === 1) {
+      stringReponse += '1 spot remaining';
+    }
+    
+    if (spots > 1) {
+      stringReponse += `${spots} spots remaining`;
+    }
+
+    return stringReponse;
+  }
   
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
