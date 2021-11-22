3b677ae57c8ebe9c9161202c889bd7d3f8ac9ae1
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-11-22 12:35:40 -0800

    Install React Hooks dev dependency. Create custom hook and add tests for it

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    
+    setHistory((prev) => {
+      const newHistory = [...prev];
+      
+      if(replace){
+        newHistory.pop()
+      }
+      
+      return [...newHistory, newMode];
+    }); 
+  }
+
+  function back() {
+    if (history.length < 2) {
+      return;
+    }
+
+    setHistory((prev) => {
+      const newHistory = [...prev];
+      newHistory.pop()
+
+      return newHistory;
+    });
+  }
+
+  const mode = history[history.length - 1];
+
+  return { mode, transition, back };
+}
