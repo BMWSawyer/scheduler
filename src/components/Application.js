7259a8e1a6aed5a7c5fada838ae418a040aacbc7
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-11-21 22:45:30 -0800

    Update state within code to fetch appointment data

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "./Appointment/";
+import { getAppointmentsForDay } from "helpers/selectors";
 import "components/Application.scss";
 
 export default function Application(props) {
@@ -11,16 +12,16 @@ export default function Application(props) {
     appointments: {}
   });
 
-  const dailyAppointments = [];
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
-  //const setAppointments = appointments => setState({ ...state, appointments });
 
   useEffect(() => {
-    axios.get("/api/days")
-    .then((res) => {
-      setDays(res.data);
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments")
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
     });
   },[]);
 
